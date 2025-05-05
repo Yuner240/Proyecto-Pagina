984adf461cadd14d17bf8ff5abe3e5e82fe0f0cc
Yuner Ch <orozcochaconyuner@gmail.com>
2025-05-05 00:29:55 -0600

    Agregar archivos de proyecto.

---
+++ Pages/register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication110.Pages
+{
+    public class registerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
